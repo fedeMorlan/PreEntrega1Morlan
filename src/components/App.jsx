4f7b3326dca24e95f96cfe53f698196a338596c8
unknown <fedexmorlan@hotmail.com>
2023-02-13 11:22:57 -0300

    reentrega por el error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
-import Dolar from './Dolar/dolar';
-import ItemCount from './ItemCount/ItemCount';
+// import Dolar from './Dolar/dolar';
+// import ItemCount from './ItemCount/ItemCount';
 import ItemListContainer from './ItemListContainer/ItemListContainer';
 import Navbar from './Navbar/Navbar';
 import ItemDetailContainer from './ItemDetailContainer/ItemDetailContainer';
 // routing
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Contacto from './Contacto/Contacto';
+
+// estilo de toastify para dar un msj
+import 'react-toastify/dist/ReactToastify.css';
+// se trata como un componente, lo puedo incluir donde yo quiera, no dentro de Routes
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
@@ -23,7 +29,9 @@ function App() {
           {/* este path va a ir cambiando, para ello se ponen los :, significa que va a cambiar */}
           <Route path='/item/:id' element={<ItemDetailContainer/>}/>
           <Route path='/category/:idCategoria' element={<ItemListContainer/>}/>
+          <Route path='/contacto' element={<Contacto/>}/>
         </Routes>
+        <ToastContainer/>
       </BrowserRouter>
     </>
   );
